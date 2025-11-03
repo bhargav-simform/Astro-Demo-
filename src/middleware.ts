7bb4e2d389a69958a8e75438cfb4c3f83de82639
Bhargav SimForm <bhargavsimform@gmail.com>
2025-11-03 10:49:24 +0530

    fix: file names fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import type { MiddlewareHandler } from "astro";
 
 const middleware: MiddlewareHandler = (context, next) => {
-    console.log("HELLO WORLD!", context.request);
     if(context.request.url.endsWith("/protected")) {
         return new Response("Access Denied", { status: 403 });
     }
